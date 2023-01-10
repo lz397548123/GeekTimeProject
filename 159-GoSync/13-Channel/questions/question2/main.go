20cc48d7dcbc1d46c1a98341f474e851fbbaeac6
kerwin <397548123@qq.com>
2023-01-10 16:38:45 +0800

    13-channel

--- 159-GoSync/13-Channel/questions/question2/main.go
+++ 159-GoSync/13-Channel/questions/question2/main.go
@@ -1,12 +1,12 @@
 package main
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(<-chan int)
-	ch3 := make(chan<- int)
-	ch4 := make(chan int)
-	ch2 = ch1
-	ch3 = ch1
-	ch4 = ch2
-	ch4 = ch3
+	//ch1 := make(chan int)
+	//ch2 := make(<-chan int)
+	//ch3 := make(chan<- int)
+	//ch4 := make(chan int)
+	//ch2 = ch1
+	//ch3 = ch1
+	//ch4 = ch2
+	//ch4 = ch3
 }
